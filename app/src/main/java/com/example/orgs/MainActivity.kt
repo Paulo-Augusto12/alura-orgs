9d6899870b43afcbfa61c71c8b65df72abb54eda
Paulo Augusto <paulo-augusto12@outlook.com>
2023-06-01 14:13:33 -0300

    chore: rendering values for the text views in layout with the activity

--- app/src/main/java/com/example/orgs/MainActivity.kt
+++ app/src/main/java/com/example/orgs/MainActivity.kt
@@ -6,10 +6,18 @@ import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 
-class MainActivity: Activity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val nome = findViewById<TextView>(R.id.nome)
+        val description = findViewById<TextView>(R.id.description)
+        val values = findViewById<TextView>(R.id.valor)
+
+        nome.text = "Cesta de frutas da Alura"
+        description.text = "Maçã, Banana, Laranja, Uva e Morango"
+        values.text = "45.80"
     }
 }
