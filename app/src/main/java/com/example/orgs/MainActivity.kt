2338bd06805df21809aee313070698787eb6a3bc
Paulo Augusto <paulo-augusto12@outlook.com>
2023-06-01 12:31:17 -0300

    feat: started view development

--- app/src/main/java/com/example/orgs/MainActivity.kt
+++ app/src/main/java/com/example/orgs/MainActivity.kt
@@ -1,7 +1,17 @@
 package com.example.orgs
 
 import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity: Activity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val view = TextView(this)
+        view.setText("Cesta de frutas")
+        setContentView(view)
+    }
 }
