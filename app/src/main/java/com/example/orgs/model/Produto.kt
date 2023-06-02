54061d58d89f5a0d6248f003fe81468c40b2464e
Paulo Augusto <paulo-augusto12@outlook.com>
2023-06-02 13:01:11 -0300

    feat: developed onCLickListener for save button

--- app/src/main/java/com/example/orgs/model/Produto.kt
+++ app/src/main/java/com/example/orgs/model/Produto.kt
@@ -2,10 +2,10 @@ package com.example.orgs.model
 
 import java.math.BigDecimal
 
-class Produto(
-    val nome: String,
-    val descricao: String,
-    val valor: BigDecimal
+data class Produto(
+    val nome: String = "",
+    val descricao: String = "",
+    val valor: BigDecimal = BigDecimal.ZERO
 
 )
 
--- app/src/main/java/com/example/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -2,7 +2,46 @@ package com.example.orgs.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import com.example.orgs.R
+import com.example.orgs.model.Produto
+import java.math.BigDecimal
 
 class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val saveButton = findViewById<Button>(R.id.saveButton)
+
+        saveButton.setOnClickListener {
+            val campoNome = findViewById<EditText>(R.id.nome)
+
+            val campoDescricao = findViewById<EditText>(R.id.descricao)
+
+            val campoValor = findViewById<EditText>(R.id.valor)
+
+            val nome = campoNome.text.toString()
+
+            val descricao = campoDescricao.text.toString()
+
+            val valorEmTexto = campoValor.text.toString()
+
+            val valor = if (valorEmTexto.isBlank()) {
+                BigDecimal.ZERO
+            } else {
+                BigDecimal(valorEmTexto)
+            }
+
+            val produtoNovo = Produto(nome = nome, descricao = descricao, valor = valor)
+
+
+            Log.i(
+                "Formulário de produto",
+                "On create: Nome: ${produtoNovo.nome} , Descrição: ${produtoNovo.descricao}, Valor: ${produtoNovo.valor}"
+            )
+        }
+    }
 }
