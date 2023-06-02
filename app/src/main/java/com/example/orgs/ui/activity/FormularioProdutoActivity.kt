65ef31394063973c8e838834d5aaafe732b57b75
Paulo Augusto <paulo-augusto12@outlook.com>
2023-06-02 11:26:08 -0300

    feat: added basics layout elements to the form activity

--- app/src/main/java/com/example/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -4,9 +4,5 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.orgs.R
 
-class FormularioProdutoActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_formulario_produto)
-    }
+class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto) {
 }
