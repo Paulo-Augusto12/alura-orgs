b2de71728d1b260363165b848f7b8de11e8ca956
Paulo Augusto <paulo-augusto12@outlook.com>
2023-06-02 13:13:41 -0300

    feat: developed floating action button and added his onCLickListener to navigate to another activity

--- app/src/main/java/com/example/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.orgs.ui.activity
 
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -9,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.orgs.R
 import com.example.orgs.model.Produto
 import com.example.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.math.BigDecimal
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
@@ -17,6 +19,15 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         super.onCreate(savedInstanceState)
 
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+
+        val floatingButton = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+
+        floatingButton.setOnClickListener {
+
+            val intent = Intent(this, FormularioProdutoActivity::class.java)
+
+            startActivity(intent)
+        }
 
         recyclerView.adapter = ListaProdutosAdapter(
             context = this,
